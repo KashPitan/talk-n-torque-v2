c409a1185c170e14e9516e0e1de4fffd01c1f6fd
KashPitan <kashpitan@googlemail.com>
2025-02-09 23:02:18 +0000

    fix: jest config module mapping and ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   preset: "react-native",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  modulePathIgnorePatterns: ["<rootDir>/supabase/functions/tests"],
+  moduleNameMapper: {
+    "^~/components/(.*)$": "<rootDir>/components/$1",
+    "^~/lib/(.*)$": "<rootDir>/lib/$1",
+  },
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
